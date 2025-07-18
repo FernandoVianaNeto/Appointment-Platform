a0a95268b38959893614223da907edb0d3714345
Fernando Viana <fernando.viana.nt@gmail.com>
2025-07-18 18:27:30 -0300

    refactor: appointment service adjustments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import Home from './pages/Appointments';
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/home" element={<Home />} />
     </Routes>
   );
--- frontend/src/core/services/appointmentsService.ts
+++ frontend/src/core/services/appointmentsService.ts
@@ -3,14 +3,10 @@ import api from './api';
 
 export async function listAppointments(): Promise<any> {
   try {
-    const res = await api.get('/appointment/list');
+    const token = localStorage.getItem('token');
 
-    const { token, user } = res.data;
-
-    localStorage.setItem('token', token);
-
-    localStorage.setItem('user', JSON.stringify(user));
-
+    const res = await api.get('/appointment/list', { headers: { 'Authorization': token }});
+    console.log('RESPONSE', res)
     return res.data;
   } catch (error: any) {
     if (error.response?.status === 401) {
