275f95880b6c83bee2127215ae9d2371e6882e4d
Fernando Viana <fernando.viana.nt@gmail.com>
2025-07-21 19:13:16 -0300

    feat: add error message in login component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/index';
-import Home from './pages/Appointments';
+import Appointments from './pages/Appointments';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/appointments" element={<Appointments />} />
     </Routes>
   );
 }
--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -2,25 +2,33 @@ import { useState } from 'react';
 import { Container, Form, Input, Button, Title } from './styles';
 import { useNavigate } from 'react-router-dom';
 import { login } from '../../core/services/authService';
+import LoadingSpinner from '../../core/components/Loading';
 
 function Login() {
   const navigate = useNavigate();
-
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [_, setError] = useState<string | null>(null);
+  const [error, setError] = useState<boolean>(false);
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
+    console.log("before preventDefault", e);
     e.preventDefault();
-    setError(null);
+    console.log("after preventDefault");
     setLoading(true);
-
+  
     try {
       await login(email, password);
-      navigate('/home');
+      navigate('/appointments');
     } catch (err: any) {
-      setError(err.message);
+      console.error('Login error:', err);
+  
+      if (err instanceof Error && err.message === 'Invalid email or password') {
+        console.log(err)
+        setError(true);
+      } else {
+        alert('Unexpected error occurred. Please try again later.');
+      }
     } finally {
       setLoading(false);
     }
@@ -28,11 +36,12 @@ function Login() {
 
   return (
     <Container>
-      <Form>
-        <Title>Appointment Plataform</Title>
-        <Input type="text" placeholder="Email" onChange={e => setEmail(e.target.value)}/>
-        <Input type="password" placeholder="Password"  onChange={e => setPassword(e.target.value)}/>
-        <Button onClick={handleSubmit}>{loading ? "Logging..." : "Login"}</Button>
+      <Form onSubmit={handleSubmit}>
+        <Title>Appointment Platform</Title>
+        <Input type="text" placeholder="Email" onChange={e => setEmail(e.target.value)} />
+        <Input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+        <Button type="submit">Login</Button>
+        {error && <p>Incorrect Email or Password</p>}
       </Form>
     </Container>
   );
